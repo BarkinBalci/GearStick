c0973e2847cbc02f60502bd8e9e3112a2088952a
ilkergzlkkr <guzelkokarilker@gmail.com>
2022-12-08 20:18:11 +0300

    chore: gitignore, authors, format-code

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
-
 import javax.swing.JFrame;
-import javax.swing.UIManager;
-
 
 public class Main {
     public static void main(String[] args) {
         JFrame mainFrame = new JFrame();
         mainFrame.setVisible(true);
+        mainFrame.toFront();
     }
 }
