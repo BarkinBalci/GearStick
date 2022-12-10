3af13aff6555d1cf0d3701470e283c239b9105ae
ilkergzlkkr <guzelkokarilker@gmail.com>
2022-12-10 22:03:04 +0300

    feat: MediaPlayer lol

--- src/main/java/com/gearstick/Main.java
+++ src/main/java/com/gearstick/Main.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("player.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("GearStick");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,11 @@
 module com.gearstick {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
 
-    requires com.dlsc.formsfx;
+    // requires com.dlsc.formsfx;
     requires org.kordamp.ikonli.javafx;
+    requires transitive javafx.graphics;
 
     opens com.gearstick to javafx.fxml;
     opens com.gearstick.controllers to javafx.fxml;
