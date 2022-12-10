4dccf92b3f67cc7020e6a0ce36563acf9846b36b
ilkergzlkkr <guzelkokarilker@gmail.com>
2022-12-11 02:12:11 +0300

    seperate fxml loader

--- src/main/java/com/gearstick/controllers/PlayerController.java
+++ src/main/java/com/gearstick/controllers/PlayerController.java
@@ -20,8 +20,7 @@ public class PlayerController {
 
     @FXML
     private void start() {
-        // File file = new File("C:\\Users\\ilker\\Videos\\Valorant\\Valorant 2022.11.04
-        // - 15.14.16.03.DVR.mp4");
+        // File file = new File("C:\\Users\\ilker\\Videos\\Valorant\\Valorant 2022.11.04 - 15.14.16.03.DVR.mp4");
         // Media media = new Media(file.toURI().toString()); //from file
         String url = "https://cdn.discordapp.com/attachments/918939943773044797/1051180857869746238/2022-12-09_15-14-51.mp4";
         Media media = new Media(url); // from web url
