8460c724bb72d03fe3b9f64a0e1c6fe6f534781a
Barkın Balcı <62614933+BarkinBalci@users.noreply.github.com>
2022-12-10 18:53:27 +0300

    Renamed Main Class & Edited .gitignore

--- src/main/java/com/gearstick/gearstick/Main.java
+++ src/main/java/com/gearstick/gearstick/Main.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
