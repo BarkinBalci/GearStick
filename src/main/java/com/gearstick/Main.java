780ce0f2950678ed80536028a7973768b246c223
Barkın Balcı <62614933+BarkinBalci@users.noreply.github.com>
2022-12-12 22:04:04 +0300

    Changed Scene to Main

--- src/main/java/com/gearstick/Main.java
+++ src/main/java/com/gearstick/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("player"));
+        scene = new Scene(loadFXML("main"));
         stage.setTitle("GearStick");
         stage.setScene(scene);
         stage.show();
