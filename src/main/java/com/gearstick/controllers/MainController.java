21b1a39ab8a368508757cb28067ae174d55427e2
ilkergzlkkr <guzelkokarilker@gmail.com>
2022-12-13 23:39:48 +0300

    demo exception handler

--- src/main/java/com/gearstick/controllers/MainController.java
+++ src/main/java/com/gearstick/controllers/MainController.java
@@ -1,10 +1,11 @@
 package com.gearstick.controllers;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import com.gearstick.AES256;
+import com.gearstick.Gears;
+import com.gearstick.Gears.EncryptionType;
+
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -21,24 +22,39 @@ public class MainController implements Initializable {
     private TextField secretKeyTextField;
     @FXML
     private TextField saltTextField;
+
     @FXML
-    private void Encrypt() throws IOException {
+    private void Encrypt() {
+        EncryptionType encryptionType = gearComboBox.getSelectionModel().getSelectedItem();
         String originalString = inputTextField.getText();
-        String encryptedString = AES256.encrypt(originalString, secretKeyTextField.getText(), saltTextField.getText());
-        outputTextField.setText(encryptedString);
-    }
-    @FXML
-    private void Decrypt() throws IOException{
-        String decryptedString = AES256.decrypt(inputTextField.getText(), secretKeyTextField.getText(), saltTextField.getText());
-        outputTextField.setText(decryptedString);
+        try {
+            String encryptedString = Gears.encrypt(originalString, secretKeyTextField.getText(),
+                    saltTextField.getText(), encryptionType);
+            outputTextField.setText(encryptedString);
+        } catch (Exception e) {
+            outputTextField.setText(e.getMessage());
+        }
     }
 
     @FXML
-    private ComboBox<String> gearComboBox;
+    private void Decrypt() {
+        try {
+            String decryptedString = Gears.decrypt(inputTextField.getText(), secretKeyTextField.getText(),
+                    saltTextField.getText());
+            outputTextField.setText(decryptedString);
+
+        } catch (Exception e) {
+            outputTextField.setText(e.getMessage());
+        }
+    }
+
+    @FXML
+    private ComboBox<EncryptionType> gearComboBox;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        gearComboBox.setItems(FXCollections.observableArrayList("AES256", "AES512", "SHA256", "SHA512"));
+        gearComboBox.setItems(FXCollections.observableArrayList(EncryptionType.AES256,
+                EncryptionType.AES512));
         gearComboBox.getSelectionModel().selectFirst();
     }
 }
