e478a4b65703bce007cc7e54fea15c9a404615c5
Barkın Balcı <62614933+BarkinBalci@users.noreply.github.com>
2023-01-02 23:29:24 +0300

    Adds Checksum.java

---
+++ src/main/java/com/gearstick/Checksum.java
@@ -0,0 +1,31 @@
+package com.gearstick;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Checksum {
+    public static String getChecksum(String digestName, File file) throws IOException, NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance(digestName);
+        FileInputStream fis = new FileInputStream(file);
+
+        byte[] byteArray = new byte[1024];
+        int bytesCount;
+
+        while ((bytesCount = fis.read(byteArray)) != -1) {
+            digest.update(byteArray, 0, bytesCount);
+        }
+
+        fis.close();
+        byte[] bytes = digest.digest();
+
+        StringBuilder sb = new StringBuilder();
+        for (byte aByte : bytes) {
+            sb.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
+        }
+
+        return sb.toString();
+    }
+}
