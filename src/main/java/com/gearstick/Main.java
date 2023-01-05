89f86657dff37a66f44c5104d5980ebf82843c79
Barkın Balcı <62614933+BarkinBalci@users.noreply.github.com>
2023-01-05 23:41:27 +0300

    Switched back to MenuBar.fxml

--- src/main/java/com/gearstick/Main.java
+++ src/main/java/com/gearstick/Main.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -12,10 +11,7 @@ import javafx.stage.StageStyle;
 import java.io.IOException;
 import java.util.HashMap;
 
-import com.gearstick.utils.EffectUtilities;
-
 public class Main extends Application {
-    public static Stage stage;
     private static Scene scene;
     private static Parent[] root;
 
@@ -29,15 +25,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        Main.stage = stage;
         scene = new Scene(new VBox());
         loadRoot();
-        EffectUtilities.makeDraggable(Main.stage, Main.getMainPane().getTop());
-
         stage.setTitle("GearStick");
+        stage.initStyle(StageStyle.UNIFIED);
+
         stage.setScene(scene);
-        stage.initStyle(StageStyle.UNDECORATED);
-        stage.initStyle(StageStyle.TRANSPARENT);
         stage.show();
     }
 
@@ -63,16 +56,13 @@ public class Main extends Application {
         setRoot(root, null);
     }
 
-    public static BorderPane getMainPane() {
-        return (BorderPane) root[0];
-    }
+
 
     private static void loadRoot() throws IOException {
         if (root == null) {
-            root = new Parent[] { loadFXML("Main"), loadFXML("Pages/Welcome") };
+            root = new Parent[] { loadFXML("MenuBar"), loadFXML("Cryptography") };
         }
-        getMainPane().setCenter(root[1]);
-        scene.setRoot(root[0]);
+        ((VBox) scene.getRoot()).getChildren().setAll(root);
     }
 
     public static Parent loadFXML(String fxml) throws IOException {
