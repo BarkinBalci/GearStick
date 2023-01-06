1a4c375a58f5fcf7556cd212155d1f10ca86575c
Barkın Balcı <62614933+BarkinBalci@users.noreply.github.com>
2023-01-06 22:37:42 +0300

    feature: checksum 👍

--- src/main/java/com/gearstick/controllers/ChecksumController.java
+++ src/main/java/com/gearstick/controllers/ChecksumController.java
@@ -1,14 +1,18 @@
 package com.gearstick.controllers;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.security.NoSuchAlgorithmException;
 import java.util.ResourceBundle;
 
+import com.gearstick.Checksum;
 import com.gearstick.Main;
 
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 
@@ -19,10 +23,18 @@ public class ChecksumController implements Initializable {
     private TextField currentFileName = new TextField("No file selected");
 
     @FXML
+    private TextArea hashTextArea = new TextArea("almk");
+
+    @FXML
     private void openFile() {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open Any File");
         currentFile.set(fileChooser.showOpenDialog(Main.scene.getWindow()));
+    }
+
+    @FXML
+    private void calcChecksum() throws IOException, NoSuchAlgorithmException {
+        hashTextArea.setText(Checksum.getChecksum("SHA-256", currentFile.get()));
     }
 
     @Override
