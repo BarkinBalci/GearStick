9c537ff0448074feb2920543b65fed718a1c0cce
Barkın Balcı <62614933+BarkinBalci@users.noreply.github.com>
2023-01-08 01:38:02 +0300

    Migrated to SecureRandom()

--- src/main/java/com/gearstick/Generator.java
+++ src/main/java/com/gearstick/Generator.java
@@ -3,38 +3,47 @@ package com.gearstick;
 import java.security.SecureRandom;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Generator {
+    /**
+     * !"#$%&'()*+,,
+     */
     public static Stream<Character> generateSpecial(int count, boolean bool) {
-        Random random = new SecureRandom();
+        SecureRandom random = new SecureRandom();
         IntStream specialChars = random.ints(count, 33, 45);
         if (!bool)
             specialChars = IntStream.empty();
         return specialChars.mapToObj(data -> (char) data);
     }
-
+    /**
+     * 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+     */
     public static Stream<Character> generateNumbers(int count, boolean bool) {
-        Random random = new SecureRandom();
+        SecureRandom random = new SecureRandom();
         IntStream numberChars = random.ints(count, 48, 57);
         if (!bool)
             numberChars = IntStream.empty();
         return numberChars.mapToObj(data -> (char) data);
     }
 
+    /**
+     * A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z
+     */
     public static Stream<Character> generateUppercase(int count, boolean bool) {
-        Random random = new SecureRandom();
+        SecureRandom random = new SecureRandom();
         IntStream uppercaseChars = random.ints(count, 65, 90);
         if (!bool)
             uppercaseChars = IntStream.empty();
         return uppercaseChars.mapToObj(data -> (char) data);
     }
-
+    /**
+     * a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
+     */
     public static Stream<Character> generateLowercase(int count, boolean bool) {
-        Random random = new SecureRandom();
+        SecureRandom random = new SecureRandom();
         IntStream lowercaseChars = random.ints(count, 97, 122);
         if (!bool)
             lowercaseChars = IntStream.empty();
